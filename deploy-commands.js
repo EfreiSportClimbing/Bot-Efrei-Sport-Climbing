42a0ae96ed847cf9fe44bf010517af27ea60af9e
adan-ea <57812567+adan-ea@users.noreply.github.com>
2022-04-19 15:01:52 +0200

    refactor(Project): Small refactor
    It shouldn't break anything (i hope)

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,59 +1,59 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+const {clientId, guildId, token} = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('seance')
-		.setDescription('Créer une nouvelle scéance')
-		.addStringOption(option => 
-			option.setName('salle')
-				.setRequired(true)
-				.setDescription('Salle de la scéance')
-				.addChoice('antreblock', 'antreblock')
-				.addChoice('arkose', 'arkose')
-				.addChoice('climb-up', 'climb-up')
-				.addChoice('vertical-art', 'vertical-art')
-		)
-		.addStringOption(option => 
-			option.setName('date')
-				.setRequired(true)
-				.setDescription('Jour de la semaine')
-				.addChoice('lundi', 'lundi')
-				.addChoice('mardi', 'mardi')
-				.addChoice('mercredi', 'mercredi')
-				.addChoice('jeudi', 'jeudi')
-				.addChoice('vendredi', 'vendredi')
-				.addChoice('samedi', 'samedi')
-				.addChoice('dimanche', 'dimanche')
-				)
-		.addStringOption(option => 
-			option.setName('heure')
-				.setRequired(true)
-				.setDescription('Heure de début')
-				.addChoice('8h', '8')
-				.addChoice('9h', '9')
-				.addChoice('10h', '10')
-				.addChoice('11h', '11')
-				.addChoice('12h', '12')
-				.addChoice('13h', '13')
-				.addChoice('14h', '14')
-				.addChoice('15h', '15')
-				.addChoice('16h', '16')
-				.addChoice('17h', '17')
-				.addChoice('18h', '18')
-				.addChoice('19h', '19')
-				.addChoice('20h', '20')
-				.addChoice('21h', '21')
-			),
-	new SlashCommandBuilder().setName('activité')
-		.setDescription('Savoir son nombre de séances')
+    new SlashCommandBuilder().setName('seance')
+        .setDescription('Créer une nouvelle scéance')
+        .addStringOption(option =>
+            option.setName('salle')
+                .setRequired(true)
+                .setDescription('Salle de la scéance')
+                .addChoice('Antreblock', 'antreblock')
+                .addChoice('Arkose', 'arkose')
+                .addChoice('Climb-up', 'climb-up')
+                .addChoice('Vertical-art', 'vertical-art')
+        )
+        .addStringOption(option =>
+            option.setName('date')
+                .setRequired(true)
+                .setDescription('Jour de la semaine')
+                .addChoice('Lundi', 'lundi')
+                .addChoice('Mardi', 'mardi')
+                .addChoice('Mercredi', 'mercredi')
+                .addChoice('Jeudi', 'jeudi')
+                .addChoice('Vendredi', 'vendredi')
+                .addChoice('Samedi', 'samedi')
+                .addChoice('Dimanche', 'dimanche')
+        )
+        .addStringOption(option =>
+            option.setName('heure')
+                .setRequired(true)
+                .setDescription('Heure de début')
+                .addChoice('8h', '8')
+                .addChoice('9h', '9')
+                .addChoice('10h', '10')
+                .addChoice('11h', '11')
+                .addChoice('12h', '12')
+                .addChoice('13h', '13')
+                .addChoice('14h', '14')
+                .addChoice('15h', '15')
+                .addChoice('16h', '16')
+                .addChoice('17h', '17')
+                .addChoice('18h', '18')
+                .addChoice('19h', '19')
+                .addChoice('20h', '20')
+                .addChoice('21h', '21')
+        ),
+    new SlashCommandBuilder().setName('activité')
+        .setDescription('Savoir son nombre de séances')
 
 ]
-	.map(command => command.toJSON());
+    .map(command => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({version: '9'}).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), {body: commands})
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const cron =  require('cron');
-const { addOne,removeOne, getOne } = require( './firestore');
-const { token, guildId, clientId } =  require('./config.json');
+const {Client, Intents, MessageEmbed} = require('discord.js');
+const cron = require('cron');
+const {addOne, removeOne, getOne} = require('./firestore');
+const {token, guildId, clientId} = require('./config.json');
 // Create a new client instance
-const client = new Client({ 
+const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 });
 
 // When the client is ready, run this code (only once)
-const reacts = ['🇦','🇧','🇨','🇩','🇪','🇫','🇬','🇭','🇮','🇯','🇰','🇱','🇲','🇳']
-let message = null
+const reacts = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯', '🇰', '🇱', '🇲', '🇳'];
+let message = null;
 const channels = [
-    {name : 'antreblock', channelId : '955472985735721010', channelEmbedId : '965621047057604648'},
-    {name : 'arkose' , channelId : "955473048444756048", channelEmbedId : '965621047133093898'},
-    {name :"climb-up", channelId : "955473017746628628", channelEmbedId : '965012209690345542'},
-    {name : "vertical-art", channelId : "955473088005431396", channelEmbedId : '965012209690374214' }
-]
+    {name: 'antreblock', channelId: '955472985735721010', channelEmbedId: '965621047057604648'},
+    {name: 'arkose', channelId: '955473048444756048', channelEmbedId: '965621047133093898'},
+    {name: 'climb-up', channelId: '955473017746628628', channelEmbedId: '965012209690345542'},
+    {name: 'vertical-art', channelId: '955473088005431396', channelEmbedId: '965012209690374214'}
+];
 
 
 const sendMessages = () => {
@@ -24,54 +24,51 @@ const sendMessages = () => {
         const embed = new MessageEmbed()
             .setTitle(`${channel.name}`)
             .setColor('GOLD')
-            .setDescription(`Horaires prévues à ${channel.name}`)
+            .setDescription(`Horaires prévus à ${channel.name}`)
             .setThumbnail('https://cdn.discordapp.com/attachments/934805065745715243/934823576807280700/Logo_ESC.png');
-        const guild = client.guilds.cache.get(guildId)
+        const guild = client.guilds.cache.get(guildId);
         const chan = guild.channels.cache.get(channel.channelId);
-        await chan.send({embeds : [embed]});
-    })
-}
+        await chan.send({embeds: [embed]});
+    });
+};
 
 const deleteSceance = (day) => {
     channels.forEach(async (channel) => {
-        const guild = client.guilds.cache.get(guildId)
+        const guild = client.guilds.cache.get(guildId);
         const chan = guild.channels.cache.get(channel.channelId);
-        const message = await channelInstance.messages.fetch(channel.channelEmbedId)
-        const embed = message.embeds[0]
-        const fields = embed.fields
-        fields = fields.filter(field => !field.name.includes(day))
-        embed.fields = fields
-        await message.edit({embeds : [embed]})
-    })
-}
-
-
-
+        const message = await channelInstance.messages.fetch(channel.channelEmbedId);
+        const embed = message.embeds[0];
+        const fields = embed.fields;
+        fields = fields.filter(field => !field.name.includes(day));
+        embed.fields = fields;
+        await message.edit({embeds: [embed]});
+    });
+};
 
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
     // sendMessages()
     let deleteMonday = new cron.CronJob('0 0 0 * * 2', () => {
-        deleteSceance('lundi')
+        deleteSceance('Lundi');
     }, null, true, 'Europe/Paris');
     let deleteTuesday = new cron.CronJob('0 0 0 * * 3', () => {
-        deleteSceance('mardi')
+        deleteSceance('Mardi');
     }, null, true, 'Europe/Paris');
     let deleteWednesday = new cron.CronJob('0 0 0 * * 4', () => {
-        deleteSceance('mercredi')
+        deleteSceance('Mercredi');
     }, null, true, 'Europe/Paris');
     let deleteThursday = new cron.CronJob('0 0 0 * * 5', () => {
-        deleteSceance('jeudi')
+        deleteSceance('Jeudi');
     }, null, true, 'Europe/Paris');
     let deleteFriday = new cron.CronJob('0 0 0 * * 6', () => {
-        deleteSceance('vendredi')
+        deleteSceance('Vendredi');
     }, null, true, 'Europe/Paris');
     let deleteSaturday = new cron.CronJob('0 0 0 * * 0', () => {
-        deleteSceance('samedi')
+        deleteSceance('Samedi');
     }, null, true, 'Europe/Paris');
     let deleteSunday = new cron.CronJob('0 0 0 * * 1', () => {
-        deleteSceance('dimanche')
+        deleteSceance('Dimanche');
     }, null, true, 'Europe/Paris');
     deleteMonday.start();
     deleteTuesday.start();
@@ -144,39 +141,38 @@ client.once('ready', () => {
 // });
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-	const { commandName } = interaction;
+    if (!interaction.isCommand()) return;
+    const {commandName} = interaction;
 
-	if (commandName === 'seance') {
+    if (commandName === 'seance') {
         const salle = interaction.options.getString('salle');
-        const date = interaction.options.getString("date");
-        const heure = interaction.options.getString("heure");
-        const messageChanel = channels.find((channel) => channel.name === salle)
-        const channelInstance = client.channels.cache.get(messageChanel.channelId)
-        const message = await channelInstance.messages.fetch(messageChanel.channelEmbedId)
-        const embed = message.embeds[0]
-        const user = interaction.user.username
-        
-        const newEmbed = new MessageEmbed(embed)
-        const field = newEmbed.fields.find((field) => field.name === `**${date}** **${heure}h**`)
+        const date = interaction.options.getString('date');
+        const heure = interaction.options.getString('heure');
+        const messageChanel = channels.find((channel) => channel.name === salle);
+        const channelInstance = client.channels.cache.get(messageChanel.channelId);
+        const message = await channelInstance.messages.fetch(messageChanel.channelEmbedId);
+        const embed = message.embeds[0];
+        const user = interaction.user.username;
+
+        const newEmbed = new MessageEmbed(embed);
+        const field = newEmbed.fields.find((field) => field.name === `**${date}** **${heure}h**`);
         if (field) {
-            if (field.value.includes(user)){
-                return interaction.reply('Vous êtes déjà inscrit à cette séance')
+            if (field.value.includes(user)) {
+                return interaction.reply({content: 'Vous êtes déjà inscrit à cette séance', ephemeral: true});
             }
-            field.value += `, *${user}*`
-        }else {
-            newEmbed.addField(`**${date}** **${heure}h**`, `*${user}*`)
+            field.value += `, *${user}*`;
+        } else {
+            newEmbed.addField(`**${date}** **${heure}h**`, `*${user}*`);
         }
-        addOne(interaction.user)
-        message.edit({embeds : [newEmbed]})
-		await interaction.reply(`Ajout d'une séance à **${salle}** le **${date}** à **${heure}h**`);
-	}
-    else if (commandName === 'activité') {
+        addOne(interaction.user);
+        message.edit({embeds: [newEmbed]});
+        await interaction.reply(`Ajout d'une séance à **${salle}** le **${date}** à **${heure}h**`);
+    } else if (commandName === 'activité') {
         getOne(interaction.user).then((activite) => {
-            interaction.reply(`Vous vous etes inscris à ${activite} séances`)
+            interaction.reply({content: `Vous vous êtes inscrits à ${activite} séances`, ephemeral: true});
         }).catch(
-            () => interaction.reply('Vous n\'êtes pas inscrit à aucune séance')
-        )
+            () => interaction.reply({content: 'Vous n\'êtes inscrit à aucune séance', ephemeral: true})
+        );
     }
 });
 
